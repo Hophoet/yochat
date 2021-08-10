8661ed81e59c3bb964f608d9a8738af5a648183a
hophoet <hophoet@gmail.com>
2021-08-10 00:01:01 +0000

    (redux) add redux actions file

---
+++ src/redux/store/actions.ts
@@ -0,0 +1,10 @@
+//Authentification
+const CLEAN: string = 'CLEAN'
+const SET_AUTH_USER_TOKEN: string = 'SET_AUTH_USER_TOKEN'
+const SET_AUTH_USER: string = 'SET_AUTH_USER'
+
+export  {
+    CLEAN,
+	SET_AUTH_USER_TOKEN,
+	SET_AUTH_USER,
+}
